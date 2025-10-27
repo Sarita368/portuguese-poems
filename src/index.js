9b1a4a0b776ff1af2c9601360aee9c93f45dd90c
Sarita <saritasilva368@gmail.com>
2025-10-27 16:08:27 +0000

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Hello World!");
+function generatePoem(event) {
+  event.preventDefault();
+
+
+ new Typewriter('.poem', {
+  strings: ['Tudo vale a pena'],
+  autoStart: true,
+  Delay: 1,
+  cursor: "",
+});
+
+ poemElement.innerHTML = "Tudo vale a pena";  
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
